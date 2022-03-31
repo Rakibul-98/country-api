ddc568f8714e0f49c44e5d8d525765f38f989468
Rakibul Hasan <rakibul.rupom2001@gmail.com>
2022-03-31 07:45:17 +0600

    displaying country details

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 // load all countries
 const loadAllCountries = () =>{
+    const detailsDiv = document.getElementById("country-details-card");
+    detailsDiv.innerHTML='';
+
     fetch("https://restcountries.com/v2/all")
     .then(res=>res.json())
     .then(data=>displayAllCountry(data))
 };
 loadAllCountries();
-
 
 // Displaying all countries
 const displayAllCountry = (countries)=>{
@@ -48,12 +50,43 @@ const loadCountryDetails = (numericCode)=>{
 
 // display country details
 const displayCountryDetails = country =>{
-    console.log(country);
+    const countryDiv = document.getElementById("country-card");
+    countryDiv.innerHTML='';
+
+    const detailsDiv = document.getElementById("country-details-card");
+    detailsDiv.innerHTML='';
+    // console.log(country);
+
+    const newDiv = document.createElement("div");
+        newDiv.innerHTML=`
+            <div class="card w-50 mx-auto my-5">
+                <div class="h-50">
+                    <img src="${country.flag}" class="card-img-top h-50" alt="...">
+                </div>
+                <div class="card-body">
+                    <h3 class="card-title">${country.name}</h3>
+                    <p class="card-text fw-bold">Country in ${country.subregion}</p>
+                    <p class="card-text"><span class="fw-bold">Capital:</span> ${country.capital}</p>
+                    <p class="card-text"><span class="fw-bold">Dialing code:</span> +${country.callingCodes[0]}</p>
+                    <p class="card-text"><span class="fw-bold">Area:</span> ${country.area} km²</p>
+                    <p class="card-text"><span class="fw-bold">Population:</span> ${country.population}</p>
+                    <p class="card-text"><span class="fw-bold">Currency: ${country.currencies[0].symbol}</span> ${country.currencies[0].name} (${country.currencies[0].code})</p>
+                    <p class="card-text"><span class="fw-bold">Nationality:</span> ${country.demonym}</p>
+                    <p class="card-text"><span class="fw-bold">Language:</span> ${country.languages[0].name}</p>
+                    <p class="card-text"><span class="fw-bold">Time Zone:</span> ${country.timezones[0]}</p>
+                    <p class="card-text"><span class="fw-bold">Member of:</span> ${country.regionalBlocs ? country.regionalBlocs[0].name : "No Organization"} ${country.regionalBlocs ? (country.regionalBlocs[0].acronym) : ""}</p>
+                </div>
+            </div>
+            <button onclick="loadAllCountries()" class="btn btn-dark mb-3">Go Back</button>
+        `;
+    detailsDiv.appendChild(newDiv);
 }
 
 // search by name
 const handleNameSearch = () =>{
-    const searchValue = document.getElementById("search-box").value;
+    const searchField = document.getElementById("search-box");
+    searchValue=searchField.value;
+    searchField.value = '';
     fetch(`https://restcountries.com/v3.1/name/${searchValue}
     `)
     .then(res=>res.json())
@@ -62,6 +95,7 @@ const handleNameSearch = () =>{
 
 const searchByFullName = (countries)=>{
     const countryDiv = document.getElementById("country-card");
+    countryDiv.innerHTML='';
     countries.forEach(country => {
         console.log(country);
         const commonName = country.name.common;
@@ -107,7 +141,10 @@ const searchByFullName = (countries)=>{
 
 // search by capital
 const handleCapitalSearch = () =>{
-    const searchValue = document.getElementById("search-box").value;
+    const searchField = document.getElementById("search-box");
+    searchValue=searchField.value;
+    searchField.value = '';
+
     fetch(`https://restcountries.com/v3.1/capital/${searchValue}
     `)
     .then(res=>res.json())
@@ -115,6 +152,9 @@ const handleCapitalSearch = () =>{
 };
 
 const searchByCapital = (countries) =>{
+    const countryDiv = document.getElementById("country-card");
+    countryDiv.innerHTML='';
+
     countries.forEach(country => {
         console.log(country);
         const commonName = country.name.common;
@@ -133,14 +173,21 @@ const searchByCapital = (countries) =>{
 
 // search by currency
 const handleCurrencySearch = () =>{
-    const searchValue = document.getElementById("search-box").value;
+    const searchField = document.getElementById("search-box");
+    searchValue=searchField.value;
+    searchField.value = '';
+
     fetch(`https://restcountries.com/v3.1/currency/${searchValue}
     `)
     .then(res=>res.json())
     .then(data=>searchByCurrency(data))
 };
 
+// display result by currency
 const searchByCurrency = (countries) =>{
+    const countryDiv = document.getElementById("country-card");
+    countryDiv.innerHTML='';
+
     countries.forEach(country => {
         console.log(country);
         const commonName = country.name.common;
@@ -159,14 +206,21 @@ const searchByCurrency = (countries) =>{
 
 // search by continent
 const handleContinentSearch = () =>{
-    const searchValue = document.getElementById("search-box").value;
+    const searchField = document.getElementById("search-box");
+    searchValue=searchField.value;
+    searchField.value = '';
+
     fetch(`https://restcountries.com/v3.1/region/${searchValue}
     `)
     .then(res=>res.json())
     .then(data=>searchByContinent(data))
 };
 
+// display result by continent
 const searchByContinent = (countries) =>{
+    const countryDiv = document.getElementById("country-card");
+    countryDiv.innerHTML='';
+
     countries.forEach(country => {
         console.log(country);
         const commonName = country.name.common;
@@ -185,14 +239,21 @@ const searchByContinent = (countries) =>{
 
 // search by organization
 const handleOrganizationSearch = () =>{
-    const searchValue = document.getElementById("search-box").value;
+    const searchField = document.getElementById("search-box");
+    searchValue=searchField.value;
+    searchField.value = '';
+
     fetch(`https://restcountries.com/v2/regionalbloc/${searchValue}
     `)
     .then(res=>res.json())
     .then(data=>searchByOrganization(data))
 };
 
+// display result organization
 const searchByOrganization = (countries) =>{
+    const countryDiv = document.getElementById("country-card");
+    countryDiv.innerHTML='';
+
     countries.forEach(country => {
         console.log(country);
         const commonName = country.name.common;
